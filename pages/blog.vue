b78852c4c9ae3e1046f0d6d609a28cab710a384b
praguzz <pratamagusta@live.com>
2023-08-07 19:36:49 +0800

    Initiate Article Listing

--- pages/blog.vue
+++ pages/blog.vue
@@ -1,7 +1,14 @@
+<script setup>
+  const contents = await queryContent('/blog').find();
+  console.log(contents);
+</script>
 <template>
   <NuxtLayout>
     <section>
       <p>This page will be displayed in /blog route.</p>
+      <div v-for="content in contents" :key="content._id">
+        <NuxtLink :to="content._path">{{ content.title}}</NuxtLink>
+      </div>
     </section>
   </NuxtLayout>
 </template>
