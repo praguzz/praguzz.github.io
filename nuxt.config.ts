488bf280b3df9d7e39acad0ba509988fd40312fb
praguzz <pratamagusta@live.com>
2023-08-07 19:46:56 +0800

    Temp Fix Deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: true,
+  nitro:{
+    prerender:{
+      crawlLinks: true,
+      failOnError: false, //This is bad!
+    }
+  },
   css: ['assets/css/main.css'],
   modules: ['@nuxt/content'],
   content: {
